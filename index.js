6ab35c8e585898a31144d030ece673857f741d9e
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-03-23 09:00:33 +0100

    install Zod n eslint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { Pool } from 'pg';
+import {z} from 'zod';
 
 dotenv.config();
 
---
+++ queries/gameQueries.js
@@ -0,0 +1,6 @@
+const playerScores = "SELECT P.name, S.score , G.title from players P JOIN scores S ON P.id = S.player_id JOIN games G ON G.id = S.game_id";
+
+
+module.exports = {
+    playerScores
+};
